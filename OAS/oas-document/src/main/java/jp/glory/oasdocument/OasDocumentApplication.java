7f76f147f9ed14bed4c2cac94e1c9247644bf4b1
Junki Yamada <junkiyamada@172-20-4-104.mcury.net>
2018-03-09 09:37:30 +0900

    プロジェクト作成

---
+++ OAS/oas-document/src/main/java/jp/glory/oasdocument/OasDocumentApplication.java
@@ -0,0 +1,12 @@
+package jp.glory.oasdocument;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OasDocumentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OasDocumentApplication.class, args);
+	}
+}
---
+++ OAS/oas-document/src/test/java/jp/glory/oasdocument/OasDocumentApplicationTests.java
@@ -0,0 +1,16 @@
+package jp.glory.oasdocument;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OasDocumentApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
