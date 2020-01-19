88765fad27f86895beead645f1426300bac0bffb
Junki Yamada <geek.junkey@gmail.com>
2020-01-19 14:27:54 +0900

    勉強対象をDockerからコンテナ周りにしたので大幅にパスの変更をした。
    また、docker-build用のDockerfileも追加した。

--- container/container-java/java-app/src/main/java/jp/glory/java/app/HomeApi.java
+++ container/container-java/java-app/src/main/java/jp/glory/java/app/HomeApi.java
@@ -1,4 +1,4 @@
-package jp.glory.docker.java.app;
+package jp.glory.java.app;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
--- container/container-java/java-app/src/test/java/jp/glory/java/app/DockerJavaAppApplicationTests.java
+++ container/container-java/java-app/src/test/java/jp/glory/java/app/DockerJavaAppApplicationTests.java
@@ -1,4 +1,4 @@
-package jp.glory.docker.java.app;
+package jp.glory.java.app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
