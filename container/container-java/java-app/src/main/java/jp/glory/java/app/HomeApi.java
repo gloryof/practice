bcf542b576c9d44cfc6276e7c28bfd5fe48cb9f1
Junki Yamada <geek.junkey@gmail.com>
2020-02-09 14:01:38 +0900

    外部パラメータを受け取れるように修正。

--- container/container-java/java-app/src/main/java/jp/glory/java/app/HomeApi.java
+++ container/container-java/java-app/src/main/java/jp/glory/java/app/HomeApi.java
@@ -1,5 +1,6 @@
 package jp.glory.java.app;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +10,34 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/home")
 public class HomeApi {
 
-    @GetMapping
-    public ResponseEntity<String> getMessage() {
+    @Value("${config.value:None}")
+    private String configValue;
 
-        return ResponseEntity.ok("OK!");
+    @Value("${param.value:None}")
+    private String paramValue;
+
+    @Value("${secret.value:None}")
+    private String secretValue;
+
+    @GetMapping
+    public ResponseEntity<Result> getMessage() {
+
+        final Result result = new Result(configValue, paramValue, secretValue);
+
+        return ResponseEntity.ok(result);
+    }
+
+    static class Result {
+        public final String configValue;
+        public final String paramValue;
+        public final String secretValue;
+
+        Result(String configValue, String paramValue, String secretValue) {
+
+            this.configValue = configValue;
+            this.paramValue = paramValue;
+            this.secretValue = secretValue;
+        }
+
     }
 }
