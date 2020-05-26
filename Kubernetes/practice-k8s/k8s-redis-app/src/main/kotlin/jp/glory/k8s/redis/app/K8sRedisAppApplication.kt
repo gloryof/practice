ff09139415e6d534f2673aaa4bbdc963d4dd336c
Junki Yamada <geek.junkey@gmail.com>
2020-05-26 19:33:16 +0900

    アクセスログを標準出力に出すように修正。

--- Kubernetes/practice-k8s/k8s-redis-app/src/main/kotlin/jp/glory/k8s/redis/app/K8sRedisAppApplication.kt
+++ Kubernetes/practice-k8s/k8s-redis-app/src/main/kotlin/jp/glory/k8s/redis/app/K8sRedisAppApplication.kt
@@ -1,11 +1,26 @@
 package jp.glory.k8s.redis.app
 
+import ch.qos.logback.access.tomcat.LogbackValve
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
 
 @SpringBootApplication
 class K8sRedisAppApplication
 
 fun main(args: Array<String>) {
 	runApplication<K8sRedisAppApplication>(*args)
+}
+
+@Configuration
+class TomcatLoggingConfig {
+	@Bean
+	fun servletContainer(): TomcatServletWebServerFactory {
+		val tomcatServletWebServerFactory = TomcatServletWebServerFactory()
+		tomcatServletWebServerFactory.addContextValves(LogbackValve())
+		return tomcatServletWebServerFactory
+	}
 }
