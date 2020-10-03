4e99e31eb2b935c457c654a697f4397ad37f4336
Junki Yamada <geek.junkey@gmail.com>
2020-10-03 14:49:18 +0900

    現状の動く物でコミット。
    OAuth周りの勉強が必要なのでここで一旦ストップ。

--- Auth/OAuth2/auth-practice-goods-app/src/main/kotlin/jp/glory/goods/app/config/spring/SecurityConfig.kt
+++ Auth/OAuth2/auth-practice-goods-app/src/main/kotlin/jp/glory/goods/app/config/spring/SecurityConfig.kt
@@ -1,43 +1,19 @@
 package jp.glory.goods.app.config.spring
 
-import jp.glory.goods.app.base.api.GoodsScope
-import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver
-import org.keycloak.adapters.springsecurity.KeycloakConfiguration
-import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.context.annotation.Bean
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper
-import org.springframework.security.web.authentication.session.NullAuthenticatedSessionStrategy
-import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 
-
-@KeycloakConfiguration
-class SecurityConfig : KeycloakWebSecurityConfigurerAdapter() {
-
-    @Autowired
-    fun configureGlobal(auth: AuthenticationManagerBuilder) {
-        val provider = keycloakAuthenticationProvider()
-        provider.setGrantedAuthoritiesMapper(SimpleAuthorityMapper())
-        auth.authenticationProvider(provider)
-    }
-
-    @Bean
-    fun keycloakConfigResolver(): KeycloakSpringBootConfigResolver =
-        KeycloakSpringBootConfigResolver()
-
-    @Bean
-    override fun sessionAuthenticationStrategy(): SessionAuthenticationStrategy =
-        NullAuthenticatedSessionStrategy()
-
+@Configuration
+class SecurityConfig : WebSecurityConfigurerAdapter() {
     override fun configure(http: HttpSecurity) {
-        super.configure(http)
         http.authorizeRequests()
-            .antMatchers("/api/goods").hasAuthority(GoodsScope.read)
+            .antMatchers("/api/**").authenticated()
             .antMatchers("/swagger-ui/**").permitAll()
             .antMatchers("/v3/api-docs/**").permitAll()
             .anyRequest()
             .denyAll()
+            .and()
+            .oauth2Login()
     }
 }
--- Auth/OAuth2/docker/README.md
+++ Auth/OAuth2/docker/README.md
@@ -1,9 +1,1 @@
 # Dockerfile置き場
-## ビルド方法
-### ビルドとプッシュ
-下記のコマンドでビルドする。  
-`targetContainer`は対象のコンテナ名を設定する。  
-
-```
-$ DOCKER_BUILDKIT=1  docker build -t localhost:5000/glory_of/oauth2-${targetContainer}:latest ${targetContainer}
-```
