3e0654eea3dff4302337b3ba71f85a413456a3a8
Junki Yamada <geek.junkey@gmail.com>
2021-01-16 17:27:33 +0900

    ある程度のドメインモデルの実装が完了したのでコミット。

--- Python/flask-practice/app/src/user/domain/model.py
+++ Python/flask-practice/app/src/user/domain/model.py
@@ -1,4 +1,17 @@
+import datetime
+
 from dataclasses import dataclass
+from dateutil.relativedelta import relativedelta
+
+class UserId:
+    pass
+
+@dataclass
+class RegisteredUserId(UserId):
+    value: int
+
+class NotRegisteredUserId(UserId):
+    pass
 
 @dataclass
 class UserName:
@@ -11,5 +24,31 @@ class UserName:
         assert len(self.lastName) > 0
         assert len(self.firstName) > 0
 
-    def get_full_name(self):
+    def get_full_name(self) -> str:
         return self.lastName + " " + self.firstName
+
+@dataclass
+class Age:
+    value: int
+
+def get_zero_age() -> int:
+    return Age(0)
+
+@dataclass
+class BirthDay:
+    value: datetime.date
+
+    def calculate_age(self) -> Age:
+        today = datetime.date.today()
+        year = relativedelta(today, self.value).years
+
+        if year < 0:
+            return get_zero_age()
+        else:
+            return Age(year)
+
+@dataclass
+class User:
+    userId: RegisteredUserId
+    userName: UserName
+    birthDay: BirthDay
--- Python/flask-practice/app/tests/context/user/domain/test_model.py
+++ Python/flask-practice/app/tests/context/user/domain/test_model.py
@@ -1,8 +1,11 @@
 import pytest
+import datetime
 
-from src.user.domain.model import UserName
+from src.user.domain.model import UserName, BirthDay
 
-class TestUserName(object):
+from dateutil.relativedelta import relativedelta
+
+class TestUserName():
 
     class TestWhenBothIsSet():
         def setup_method(self):
@@ -27,3 +30,61 @@ class TestUserName(object):
         def test_when_first_name_is_none(self):
             with pytest.raises(AssertionError):
                 UserName("テスト", None)
+
+class TestBirthDay():
+
+    class TestWhenSameDateToday():
+        def setup_method(self):
+            value = datetime.date.today() - relativedelta(years=20)
+            self.sut = BirthDay(value)
+
+        def test_calculate_age(self):
+            assert 20 == self.sut.calculate_age().value
+
+    class TestWhenSameDateYesterday():
+        def setup_method(self):
+            value = datetime.date.today() - relativedelta(years=20, days=1)
+            self.sut = BirthDay(value)
+
+        def test_calculate_age(self):
+            assert 20 == self.sut.calculate_age().value
+
+    class TestWhenSameDateTomorrow():
+        def setup_method(self):
+            value = datetime.date.today() - relativedelta(years=20) + relativedelta(days=1)
+            self.sut = BirthDay(value)
+
+        def test_calculate_age(self):
+            assert 19 == self.sut.calculate_age().value
+
+    class TestWhenToday():
+        def setup_method(self):
+            value = datetime.date.today()
+            self.sut = BirthDay(value)
+
+        def test_calculate_age(self):
+            assert 0 == self.sut.calculate_age().value
+
+    class TestWhenYesterday():
+        def setup_method(self):
+            value = datetime.date.today() - relativedelta(days=1)
+            self.sut = BirthDay(value)
+
+        def test_calculate_age(self):
+            assert 0 == self.sut.calculate_age().value
+
+    class TestWhenTomorrow():
+        def setup_method(self):
+            value = datetime.date.today() + relativedelta(days=1)
+            self.sut = BirthDay(value)
+
+        def test_calculate_age(self):
+            assert 0 == self.sut.calculate_age().value
+
+    class TestWhenFutureOver1Year():
+        def setup_method(self):
+            value = datetime.date.today() + relativedelta(years=1)
+            self.sut = BirthDay(value)
+
+        def test_calculate_age(self):
+            assert 0 == self.sut.calculate_age().value
