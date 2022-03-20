4cb20faa560b67c965bd279c6f18172c74d0c11f
Junki Yamada <geek.junkey@gmail.com>
2022-03-20 17:22:25 +0900

    DroneからNewman経由でテストが成功したのでコミット。

--- CI-CD/drone/ci-cd-practice-app/drone-it.sh
+++ CI-CD/drone/ci-cd-practice-app/drone-it.sh
@@ -1,6 +1,7 @@
 # !/bin/sh
 source ./local-file/local.env
 
-cp -a ./config/zap /tmp
+cp -a ./config/postman /tmp
+# cp -a ./config/zap /tmp
 
 drone exec --pipeline integration-test --env-file ./local-file/local.env --trusted
