1049d14316bd06ad89b7a4cad84575489c7bd264
Junki Yamada <geek.junkey@gmail.com>
2022-08-03 10:58:48 +0900

    起動と終了コマンドを変更。

--- Chaos-Mesh/first-practice/k8s/start.sh
+++ Chaos-Mesh/first-practice/k8s/start.sh
@@ -2,5 +2,5 @@
 kubectl apply -f namespace.yaml
 kubectl apply --recursive -f web
 kubectl apply --recursive -f app
-kubectl apply --recursive -f chaos-mesh
 kubectl apply --recursive -f zipkin
+kubectl apply --recursive -f chaos-mesh
--- Chaos-Mesh/first-practice/k8s/stop.sh
+++ Chaos-Mesh/first-practice/k8s/stop.sh
@@ -2,4 +2,5 @@
 
 kubectl delete --recursive -f app
 kubectl delete --recursive -f web
+kubectl delete --recursive -f zipkin
 kubectl delete --recursive -f chaos-mesh
