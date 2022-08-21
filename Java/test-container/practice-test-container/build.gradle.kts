61f3b8886b6b87161a37c78d72244ac8d0a4d19d
Junki Yamada <geek.junkey@gmail.com>
2022-08-21 13:42:46 +0900

    ログの出力処理を追加。

--- Java/test-container/practice-test-container/build.gradle.kts
+++ Java/test-container/practice-test-container/build.gradle.kts
@@ -19,6 +19,11 @@ repositories {
 dependencies {
     implementation("io.lettuce:lettuce-core:6.2.0.RELEASE")
 
+    implementation("org.slf4j:slf4j-api:1.7.36")
+    implementation("ch.qos.logback:logback-core:1.2.11")
+    implementation("ch.qos.logback:logback-classic:1.2.11")
+
+
     testImplementation(platform("org.junit:junit-bom:5.9.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.testcontainers:testcontainers:1.17.3")
--- Java/test-container/practice-test-container/src/test/kotlin/jp/glory/pratice/test_container/redis/RedisCacheClientTest.kt
+++ Java/test-container/practice-test-container/src/test/kotlin/jp/glory/pratice/test_container/redis/RedisCacheClientTest.kt
@@ -1,9 +1,13 @@
 package jp.glory.pratice.test_container.redis
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.testcontainers.containers.GenericContainer
+import org.testcontainers.containers.output.Slf4jLogConsumer
 import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
 import org.testcontainers.utility.DockerImageName
@@ -11,10 +15,17 @@ import org.testcontainers.utility.DockerImageName
 @Testcontainers
 internal class RedisCacheClientTest {
     private val targetPort = 6379
+    private val logger: Logger = LoggerFactory.getLogger(this::class.java)
+    private val consumer: Slf4jLogConsumer = Slf4jLogConsumer(logger)
 
     @Container
     private val redis: GenericContainer<*> = GenericContainer(DockerImageName.parse("redis:alpine:7.0.4-alpine"))
         .withExposedPorts(targetPort)
+
+    @BeforeEach
+    fun prepareContainer() {
+        redis.followOutput(consumer)
+    }
 
     @Nested
     inner class TestSet {
