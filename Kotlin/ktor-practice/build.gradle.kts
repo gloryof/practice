d5536c2a9177f37fb061fc02ed2404b877c68a40
Junki Yamada <geek.junkey@gmail.com>
2023-07-17 14:51:11 +0900

    コミット漏れ。

---
+++ Kotlin/ktor-practice/build.gradle.kts
@@ -0,0 +1,56 @@
+val ktor_version: String by project
+val kotlin_version: String by project
+val logback_version: String by project
+val prometeus_version : String by project
+val koin_version : String by project
+val ktor_koin_version : String by project
+
+plugins {
+    kotlin("jvm") version "1.9.0"
+    id("io.ktor.plugin") version "2.3.2"
+}
+
+group = "jp.glory"
+version = "0.0.1"
+application {
+    mainClass.set("com.example.ApplicationKt")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // Ktor
+    implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-auth:$ktor_version")
+
+    implementation("io.ktor:ktor-serialization-jackson-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-metrics-micrometer-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-metrics-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
+
+    // Koin
+    implementation("io.insert-koin:koin-core:$koin_version")
+    implementation("io.insert-koin:koin-ktor:$ktor_koin_version")
+
+    // Micrometer
+    implementation("io.micrometer:micrometer-registry-prometheus:$prometeus_version")
+
+    // Jackson
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.15.2")
+
+    // kotlin-result
+    implementation("com.michael-bull.kotlin-result:kotlin-result:1.1.16")
+
+    // Logback
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+
+
+    testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+}
