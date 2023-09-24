24fc727616a5cdcef4b516a3a31c1ab0abde45c2
Junki Yamada <geek.junkey@gmail.com>
2023-09-24 14:34:06 +0900

    可視性を変更。

--- Java/java-playground/src/main/java/jp/glory/java/playground/v21/pattern/Shape.java
+++ Java/java-playground/src/main/java/jp/glory/java/playground/v21/pattern/Shape.java
@@ -1,5 +1,5 @@
 package jp.glory.java.playground.v21.pattern;
 
-sealed interface Shape permits Circle, Square {
+public sealed interface Shape permits Circle, Square {
     double calculateArea();
 }
