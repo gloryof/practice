b234e89206a998095edb68d092f6e3924453eb16
Junki Yamada <geek.junkey@gmail.com>
2023-10-29 14:37:34 +0900

    CSVのダウンロード処理を追加した。

--- Java/Spring-Boot/boot3-practice/src/main/kotlin/jp/glory/boot3practice/user/adaptor/web/UserApi.kt
+++ Java/Spring-Boot/boot3-practice/src/main/kotlin/jp/glory/boot3practice/user/adaptor/web/UserApi.kt
@@ -5,10 +5,14 @@ import jp.glory.boot3practice.base.adaptor.web.EndpointConst
 import jp.glory.boot3practice.base.adaptor.web.WebApi
 import jp.glory.boot3practice.base.adaptor.web.WebExceptionHelper
 import jp.glory.boot3practice.user.use_case.FindUserUseCase
+import org.springframework.core.io.InputStreamResource
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import reactor.core.publisher.Mono
+import java.io.ByteArrayInputStream
+import java.io.InputStream
 import java.time.LocalDate
 
 @WebApi
@@ -28,6 +32,13 @@ class UserApi(
         Mono.just(findUserUseCase.findAllUsers())
             .map { toUsersResponse(it) }
             .map { UsersResponse(it) }
+
+    @GetMapping("/csv")
+    fun getUsersByCsv(): Mono<ResponseEntity<InputStreamResource>> =
+        Mono.just(findUserUseCase.findAllUsers())
+            .map { toCsvLines(it) }
+            .map { toInputStream(it) }
+            .map { ResponseEntity.ok().body(InputStreamResource(it)) }
 
     @GetMapping("/{id}")
     fun getById(
@@ -55,4 +66,19 @@ class UserApi(
             name = output.name,
             birthDay = output.birthDay
         )
+
+    private fun toCsvLines(output: FindUserUseCase.UsersOutput): List<String> {
+        val firstLine = "no,name,birth-day"
+        val lines = mutableListOf(firstLine)
+
+        output.users
+            .mapIndexed { index, user -> "$index,${user.name},${user.birthDay}" }
+            .let { lines.addAll(it) }
+
+        return lines
+    }
+
+    private fun toInputStream(lines:List<String>): InputStream =
+        lines.joinToString("\n")
+            .let { ByteArrayInputStream(it.toByteArray()) }
 }
