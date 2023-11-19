aa9cc729a4690b6bbe772a514b04716ae25f686e
Junki Yamada <geek.junkey@gmail.com>
2023-11-19 13:52:04 +0900

    コミット漏れ。

---
+++ Measurement/practice-measure-architecture/src/main/kotlin/jp/glory/practice/measure/architecture/PracticeMeasureArchitectureApplication.kt
@@ -0,0 +1,11 @@
+package jp.glory.practice.measure.architecture
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PracticeMeasureArchitectureApplication
+
+fun main(args: Array<String>) {
+	runApplication<PracticeMeasureArchitectureApplication>(*args)
+}
---
+++ Measurement/practice-measure-architecture/src/test/kotlin/jp/glory/practice/measure/architecture/PracticeMeasureArchitectureApplicationTests.kt
@@ -0,0 +1,13 @@
+package jp.glory.practice.measure.architecture
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PracticeMeasureArchitectureApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
