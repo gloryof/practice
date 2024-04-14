feca89faf6e20aa186ba2e2c4642b72f2353778c
Junki Yamada <geek.junkey@gmail.com>
2024-04-14 14:41:50 +0900

    テスト用の初期ユーザを追加。

--- full-stack/practice-full-stack/practice-fullstack-server/src/main/kotlin/jp/glory/practice/fullstack/server/base/adaptor/store/AuthDao.kt
+++ full-stack/practice-full-stack/practice-fullstack-server/src/main/kotlin/jp/glory/practice/fullstack/server/base/adaptor/store/AuthDao.kt
@@ -2,6 +2,12 @@ package jp.glory.practice.fullstack.server.base.adaptor.store
 
 class AuthDao {
     private val auths = mutableMapOf<String, AuthRecord>()
+        .also {
+            it["test-user-id"] = AuthRecord(
+                userId = "test-user-id",
+                password = "test-password"
+            )
+        }
 
     fun findById(value: String): AuthRecord? = auths[value]
     fun save(record: AuthRecord) {
--- full-stack/practice-full-stack/practice-fullstack-server/src/main/kotlin/jp/glory/practice/fullstack/server/base/adaptor/store/UserDao.kt
+++ full-stack/practice-full-stack/practice-fullstack-server/src/main/kotlin/jp/glory/practice/fullstack/server/base/adaptor/store/UserDao.kt
@@ -4,6 +4,14 @@ import java.time.LocalDate
 
 class UserDao {
     private val users = mutableMapOf<String, UserRecord>()
+        .also {
+            it["test-user-id"] = UserRecord(
+                id = "test-user-id",
+                name = "test-user",
+                birthday = LocalDate.now()
+            )
+        }
+
 
     fun findById(value: String): UserRecord? = users[value]
     fun save(record: UserRecord) {
