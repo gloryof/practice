ea3d62828878e513d9b2fb32f914f58485fec109
Junki Yamada <geek.junkey@gmail.com>
2024-05-04 10:58:57 +0900

    Kotlin版に変更。

---
+++ OpenFeature/practice-flagd/src/main/kotlin/jp/glory/practice/flagd/PracticeFlagdApplication.kt
@@ -0,0 +1,11 @@
+package jp.glory.practice.flagd
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PracticeFlagdApplication
+
+fun main(args: Array<String>) {
+	runApplication<PracticeFlagdApplication>(*args)
+}
