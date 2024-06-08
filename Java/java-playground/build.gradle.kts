d607fb233d2460744259389d844d1802cbc796cd
Junki Yamada <geek.junkey@gmail.com>
2024-06-08 15:19:45 +0900

    Gradleのバージョンをアップデート。

--- Java/java-playground/build.gradle.kts
+++ Java/java-playground/build.gradle.kts
@@ -25,10 +25,10 @@ dependencies {
 // Apply a specific Java toolchain to ease working on different environments.
 java {
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(21))
+        languageVersion.set(JavaLanguageVersion.of(22))
     }
 
-    sourceCompatibility = JavaVersion.VERSION_21
+    sourceCompatibility = JavaVersion.VERSION_22
 
 }
 
