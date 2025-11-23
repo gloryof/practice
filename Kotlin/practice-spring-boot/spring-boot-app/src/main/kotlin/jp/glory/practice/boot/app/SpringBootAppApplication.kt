e92cbe0570e7e11c1fdb83a9b398b5a18fe4adee
Junki Yamada <geek.junkey@gmail.com>
2025-11-23 13:00:24 +0900

    Spring Boot練習用のアプリを追加。

---
+++ Kotlin/practice-spring-boot/spring-boot-app/src/main/kotlin/jp/glory/practice/boot/app/SpringBootAppApplication.kt
@@ -0,0 +1,11 @@
+package jp.glory.practice.boot.app
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootAppApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringBootAppApplication>(*args)
+}
